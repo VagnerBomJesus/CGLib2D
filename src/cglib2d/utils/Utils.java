18b31181d579fb466017d04018609dfb24f8ea04
Vagner Bom Jesus <vagneripg@gmail.com>
2020-11-09 17:42:43 +0000

    Class para importar imagem

---
+++ src/cglib2d/utils/Utils.java
@@ -0,0 +1,25 @@
+package cglib2d.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+public class Utils {
+	//para caregager imagem para a memoria 
+	public static BufferedImage readImage(Object obj, String imageFileName) {
+		BufferedImage bi = null;
+		URL imageSrc = null;
+		
+		imageSrc = obj.getClass().getClassLoader().getResource(imageFileName);
+		try {
+			bi = ImageIO.read(imageSrc);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Image could not be read");
+		}
+		
+		return bi;
+	}
+}
